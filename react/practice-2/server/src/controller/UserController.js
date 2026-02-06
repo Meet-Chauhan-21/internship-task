87383da59b20de4638393ca8a0c29d2c108965df
meet chauhan <meetchauhan9915@gmail.com>
2026-02-06 10:13:15 +0530

    change backend

--- react/practice-2/server/src/controller/UserController.js
+++ react/practice-2/server/src/controller/UserController.js
@@ -1,3 +1,4 @@
+import UserModel from "../model/User.js"
 import { createNewUser, deleteUserById, getAlluser, updateUserById } from "../services/UserServices.js"
 import { createUserValidation } from "../validation/validation.js"
 
@@ -9,30 +10,82 @@ export const getUser = async (req,res) => {
 export const createUser = async (req,res) =>{
 
     try{
-        // validate valid user
-        const {error} = createUserValidation.validate(req.body);
-        if(error){
-            console.log("error === ",error)
-            return res.send(error)
+
+        // check email
+        const checkEmail = await UserModel.findOne({email:req.body.email})
+        if(checkEmail){
+            return res.status(400).json({message : "this email already exiest..!"})
         }
 
-        const users = await createNewUser(req.body)
-        res.send(users)
+        // check phone
+        const checkPhone = await UserModel.findOne({phone: req.body.phone})
+        if(checkPhone){
+            return res.status(400).json({message : "this phone number already exiest..!"})
+        }
+
+        const user = await UserModel.create(req.body)
+        if(user){
+            return res.status(200).json({
+                user,
+                message:"user successfully created.!"
+            })
+        }
         
     } catch(e){
-        console.log(e)
+
+        if(e.name === "ValidationError"){
+            const error = Object.values(e.errors).map(err => err.message)
+            return res.status(400).json({
+                message:"validation error",
+                error
+            })
+        }
+        
+        res.status(500).json({
+            message : "server error",
+            e
+        })
+
     }
+
 }
 
 export const updateUser = async (req,res) =>{
+    
+    // check email
+        // const checkEmail = await UserModel.findOne({email:req.body.email})
+        // if(checkEmail){
+        //     return res.status(400).json({message : "this email already exiest..!"})
+        // }
 
-    const users = await updateUserById(req.body)
-    res.send(users)
+    // check phone
+        // const checkPhone = await UserModel.findOne({phone: req.body.phone})
+        // if(checkPhone){
+        //     return res.status(400).json({message : "this phone number already exiest..!"})
+        // }
+
+        const user = await UserModel.updateOne(
+            { _id: req.body._id },
+            { $set: req.body }
+        )
+        if(user){
+            return res.status(200).json({
+                user,
+                message:"user successfully updated.!"
+            })
+        }
+    
 }
 
 export const deleteUser = async (req,res) =>{
-    const users = await deleteUserById(req.body)
-    res.send(users)
+    const user = await UserModel.deleteOne({ _id: req.body._id })
+        if(user){
+            return res.status(200).json({
+                user,
+                message:"user successfully deleted.!"
+            })
+        }
+
 }
 
 
