d1232733730f5b0340792550fb5d8c14b13c23e7
meet chauhan <meetchauhan9915@gmail.com>
2026-03-03 09:48:59 +0530

    understand useCallback hook

---
+++ react/Hooks/src/components/UseReducer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UseReducer = () => {
+  return (
+        <div>
+        <h2 className='text-center m-3 text-red-400 font-mono text-xl'>useReducer hook explain</h2>
+    </div>
+  )
+}
+
+export default UseReducer
